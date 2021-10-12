524b5aaa7f2392002df54035e8e900546d9e46c6
Cahyo Waskito Hadi <48090870+cahyoh76@users.noreply.github.com>
2021-10-12 23:39:17 +0700

    Add files via upload

---
+++ akademik.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 12 Okt 2021 pada 18.38
+-- Versi server: 10.1.30-MariaDB
+-- Versi PHP: 7.2.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` varchar(5) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jkel` varchar(1) DEFAULT NULL,
+  `alamat` text,
+  `tgllhr` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `jkel`, `alamat`, `tgllhr`) VALUES
+('MH02', 'Rita', 'P', 'SOLO', '1999-01-01'),
+('MH03', 'Amirudin', 'L', 'SEMARANG', '1998-08-11'),
+('MH04', 'Siti Maryam', 'P', 'JAKARTA', '1995-04-15'),
+('MHS01', 'Siti Aminah', 'P', 'SOLO', '1995-10-01');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
